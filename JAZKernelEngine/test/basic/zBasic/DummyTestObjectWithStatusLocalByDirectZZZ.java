d50e451c107896f509ac2d42f8c936b4e948fc85
firak01 <firak01@t-online.de>
2026-05-11 22:28:53 +0200

    Am Enum Handling was geändert

--- JAZKernelEngine/test/basic/zBasic/DummyTestObjectWithStatusLocalByDirectZZZ.java
+++ JAZKernelEngine/test/basic/zBasic/DummyTestObjectWithStatusLocalByDirectZZZ.java
@@ -198,212 +198,212 @@ public class DummyTestObjectWithStatusLocalByDirectZZZ extends AbstractObjectWit
 	}
 	
 	//####################################
-		//### STATUS: ILogFileWatchMonitorZZZ
-		//####################################
+	//### STATUS: ILogFileWatchMonitorZZZ
+	//####################################
 
-		//####### aus IStatusLocalUserZZZ
-		@Override
-		public boolean getStatusLocal(Enum objEnumStatusIn) throws ExceptionZZZ {
-			boolean bFunction = false;
-			main:{
-				if(objEnumStatusIn==null) break main;
-				
-				IDummyTestObjectWithStatusLocalByInterfaceZZZ.STATUSLOCAL enumStatus = (IDummyTestObjectWithStatusLocalByInterfaceZZZ.STATUSLOCAL) objEnumStatusIn;
-				String sStatusName = enumStatus.name();
-				if(StringZZZ.isEmpty(sStatusName)) break main;
-											
-				HashMap<String, Boolean> hmFlag = this.getHashMapStatusLocal();
-				Boolean objBoolean = hmFlag.get(sStatusName.toUpperCase());
-				if(objBoolean==null){
-					bFunction = false;
-				}else{
-					bFunction = objBoolean.booleanValue();
-				}
-								
-			}	// end main:
+	//####### aus IStatusLocalUserZZZ
+	@Override
+	public boolean getStatusLocal(Enum objEnumStatusIn) throws ExceptionZZZ {
+		boolean bFunction = false;
+		main:{
+			if(objEnumStatusIn==null) break main;
 			
-			return bFunction;	
-		}
-
-		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-		//+++ OFFER STATUS LOCAL, alle Varianten, gecasted auf dieses Objekt
-		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-		
-		@Override
-		public boolean offerStatusLocal(Enum enumStatusIn, boolean bStatusValue) throws ExceptionZZZ {
-			boolean bFunction = false;
-			main:{
-				if(enumStatusIn==null) break main;
-				
-				IDummyTestObjectWithStatusLocalByInterfaceZZZ.STATUSLOCAL enumStatus = (IDummyTestObjectWithStatusLocalByInterfaceZZZ.STATUSLOCAL) enumStatusIn;
-				
-				bFunction = this.offerStatusLocal_(enumStatus, "", bStatusValue);				
-			}//end main;
-			return bFunction;
-		}
-		
-		
-		@Override
-		public boolean offerStatusLocal(Enum enumStatusIn, boolean bStatusValue, String sStatusMessage) throws ExceptionZZZ {
-			boolean bFunction = false;
-			main:{
-				if(enumStatusIn==null) break main;
-				
-				IDummyTestObjectWithStatusLocalByInterfaceZZZ.STATUSLOCAL enumStatus = (IDummyTestObjectWithStatusLocalByInterfaceZZZ.STATUSLOCAL) enumStatusIn;
-				
-				bFunction = this.offerStatusLocal_(enumStatus, sStatusMessage, bStatusValue);				
-			}//end main;
-			return bFunction;
-		}
-		
-		private boolean offerStatusLocal_(Enum enumStatusIn, String sStatusMessage, boolean bStatusValue) throws ExceptionZZZ {
-			boolean bFunction = false;
-			main:{
-				if(enumStatusIn==null) break main;
-				
-			
-				//Merke: In anderen Klassen, die dieses Design-Pattern anwenden ist das eine andere Klasse fuer das Enum
-				IDummyTestObjectWithStatusLocalByInterfaceZZZ.STATUSLOCAL enumStatus = (IDummyTestObjectWithStatusLocalByInterfaceZZZ.STATUSLOCAL) enumStatusIn;
-				String sStatusName = enumStatus.name();
-				bFunction = this.proofStatusLocalExists(sStatusName);															
-				if(!bFunction) {
-					String sLog = ReflectCodeZZZ.getPositionCurrent() + "Would like to fire event, but this status is not available: '" + sStatusName + "'";
-					this.logProtocol(sLog);			
-					break main;
-				}
-				
-			bFunction = this.proofStatusLocalValueChanged(sStatusName, bStatusValue);
-			if(!bFunction) {
-				String sLog = ReflectCodeZZZ.getPositionCurrent() + "Would like to fire event, but this status has not changed: '" + sStatusName + "'";
-				this.logProtocol(sLog);
-				break main;
-			}	
-			
-			//++++++++++++++++++++	
-			//Setze den Status nun in die HashMap
-			HashMap<String, Boolean> hmStatus = this.getHashMapStatusLocal();
-			hmStatus.put(sStatusName.toUpperCase(), bStatusValue);
-			
-			//Den enumStatus als currentStatus im Objekt speichern...
-			//                   dito mit dem "vorherigen Status"
-			//Setze nun das Enum, und damit auch die Default-StatusMessage
-			String sStatusMessageToSet = null;
-			if(StringZZZ.isEmpty(sStatusMessage)){
-				if(bStatusValue) {
-					sStatusMessageToSet = enumStatus.getStatusMessage();
-				}else {
-					sStatusMessageToSet = "NICHT " + enumStatus.getStatusMessage();
-				}			
-			}else {
-				sStatusMessageToSet = sStatusMessage;
+			IDummyTestObjectWithStatusLocalByInterfaceZZZ.STATUSLOCAL enumStatus = (IDummyTestObjectWithStatusLocalByInterfaceZZZ.STATUSLOCAL) objEnumStatusIn;
+			String sStatusName = enumStatus.name();
+			if(StringZZZ.isEmpty(sStatusName)) break main;
+										
+			HashMap<String, Boolean> hmFlag = this.getHashMapStatusLocal();
+			Boolean objBoolean = hmFlag.get(sStatusName.toUpperCase());
+			if(objBoolean==null){
+				bFunction = false;
+			}else{
+				bFunction = objBoolean.booleanValue();
 			}
-			
-			String sLog = ReflectCodeZZZ.getPositionCurrent() + "Verarbeite sStatusMessageToSet='" + sStatusMessageToSet + "'";
-			this.logProtocol(sLog);
-
-			//Falls eine Message extra uebergeben worden ist, ueberschreibe...
-			if(sStatusMessageToSet!=null) {
-				sLog = ReflectCodeZZZ.getPositionCurrent() + "Setze sStatusMessageToSet='" + sStatusMessageToSet + "'";
-				this.logProtocol(sLog);
-			}
-			//Merke: Dabei wird die uebergebene Message in den speziellen "Ringspeicher" geschrieben, auch NULL Werte...
-			this.offerStatusLocalEnum(enumStatus, bStatusValue, sStatusMessageToSet);
-			
-			
-			
-			//Falls irgendwann ein Objekt sich fuer die Eventbenachrichtigung registriert hat, gibt es den EventBroker.
-			//Dann erzeuge den Event und feuer ihn ab.	
-			if(this.getSenderStatusLocalUsed()==null) {
-				sLog = ReflectCodeZZZ.getPositionCurrent() + "Would like to fire event '" + enumStatus.getAbbreviation() + "', but no objEventStatusLocalBroker available, any registered?";
-				this.logProtocol(sLog);		
-				break main;
-			}
-			
-			//Erzeuge fuer das Enum einen eigenen Event. Die daran registrierten Klassen koennen in einer HashMap definieren, ob der Event fuer sie interessant ist.		
-			sLog = ReflectCodeZZZ.getPositionCurrent() + "Erzeuge Event fuer '" + sStatusName + "'";		
-			this.logProtocol(sLog);
-			IEventObjectStatusLocalZZZ event = new EventObjectStatusLocalZZZ(this,enumStatus, bStatusValue);			
-			
-			//### GGFS. noch weitere benoetigte Objekte hinzufuegen............
-			//...
-			
-					
-			//Feuere den Event ueber den Broker ab.
-			sLog = ReflectCodeZZZ.getPositionCurrent() + "Fires event '" + enumStatus.getAbbreviation() + "'";
-			this.logProtocol(sLog);
-			this.getSenderStatusLocalUsed().fireEvent(event);
-					
-			bFunction = true;				
+							
 		}	// end main:
+		
+		return bFunction;	
+	}
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	//+++ OFFER STATUS LOCAL, alle Varianten, gecasted auf dieses Objekt
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	
+	@Override
+	public boolean offerStatusLocal(Enum enumStatusIn, boolean bStatusValue) throws ExceptionZZZ {
+		boolean bFunction = false;
+		main:{
+			if(enumStatusIn==null) break main;
+			
+			IDummyTestObjectWithStatusLocalByInterfaceZZZ.STATUSLOCAL enumStatus = (IDummyTestObjectWithStatusLocalByInterfaceZZZ.STATUSLOCAL) enumStatusIn;
+			
+			bFunction = this.offerStatusLocal_(enumStatus, "", bStatusValue);				
+		}//end main;
 		return bFunction;
-		}
-		
-		
-		
-		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-		//+++ SET STATUS LOCAL, alle Varianten, gecasted auf dieses Objekt
-		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-		@Override
-		public boolean setStatusLocal(Enum enumStatusIn, boolean bStatusValue) throws ExceptionZZZ {
-			boolean bFunction = false;
-			main:{
-				if(enumStatusIn==null) break main;
-				
-				IDummyTestObjectWithStatusLocalByInterfaceZZZ.STATUSLOCAL enumStatus = (IDummyTestObjectWithStatusLocalByInterfaceZZZ.STATUSLOCAL) enumStatusIn;
-								
-				bFunction = this.offerStatusLocal(enumStatus, bStatusValue, null);
-			}//end main:
-			return bFunction;
-		}
+	}
+	
+	
+	@Override
+	public boolean offerStatusLocal(Enum enumStatusIn, boolean bStatusValue, String sStatusMessage) throws ExceptionZZZ {
+		boolean bFunction = false;
+		main:{
+			if(enumStatusIn==null) break main;
 			
-		/* (non-Javadoc)
-		 * @see basic.zBasic.AbstractObjectWithStatusZZZ#setStatusLocalEnum(basic.zBasic.util.abstractEnum.IEnumSetMappedStatusZZZ, boolean)
-		 */
-		@Override 
-		public boolean setStatusLocalEnum(IEnumSetMappedStatusLocalZZZ enumStatusIn, boolean bStatusValue) throws ExceptionZZZ {
-			boolean bReturn = false;
-			main:{
-				if(enumStatusIn==null) break main;
-
-				IDummyTestObjectWithStatusLocalByInterfaceZZZ.STATUSLOCAL enumStatus = (IDummyTestObjectWithStatusLocalByInterfaceZZZ.STATUSLOCAL) enumStatusIn;
-				
-				bReturn = this.offerStatusLocal(enumStatus, bStatusValue, null);
-			}//end main:
-			return bReturn;
+			IDummyTestObjectWithStatusLocalByInterfaceZZZ.STATUSLOCAL enumStatus = (IDummyTestObjectWithStatusLocalByInterfaceZZZ.STATUSLOCAL) enumStatusIn;
+			
+			bFunction = this.offerStatusLocal_(enumStatus, sStatusMessage, bStatusValue);				
+		}//end main;
+		return bFunction;
+	}
+	
+	private boolean offerStatusLocal_(Enum enumStatusIn, String sStatusMessage, boolean bStatusValue) throws ExceptionZZZ {
+		boolean bFunction = false;
+		main:{
+			if(enumStatusIn==null) break main;
+			
+		
+			//Merke: In anderen Klassen, die dieses Design-Pattern anwenden ist das eine andere Klasse fuer das Enum
+			IDummyTestObjectWithStatusLocalByInterfaceZZZ.STATUSLOCAL enumStatus = (IDummyTestObjectWithStatusLocalByInterfaceZZZ.STATUSLOCAL) enumStatusIn;
+			String sStatusName = enumStatus.name();
+			bFunction = this.proofStatusLocalExists(sStatusName);															
+			if(!bFunction) {
+				String sLog = ReflectCodeZZZ.getPositionCurrent() + "Would like to fire event, but this status is not available: '" + sStatusName + "'";
+				this.logProtocol(sLog);			
+				break main;
+			}
+			
+		bFunction = this.proofStatusLocalValueChanged(sStatusName, bStatusValue);
+		if(!bFunction) {
+			String sLog = ReflectCodeZZZ.getPositionCurrent() + "Would like to fire event, but this status has not changed: '" + sStatusName + "'";
+			this.logProtocol(sLog);
+			break main;
+		}	
+		
+		//++++++++++++++++++++	
+		//Setze den Status nun in die HashMap
+		HashMap<String, Boolean> hmStatus = this.getHashMapStatusLocal();
+		hmStatus.put(sStatusName.toUpperCase(), bStatusValue);
+		
+		//Den enumStatus als currentStatus im Objekt speichern...
+		//                   dito mit dem "vorherigen Status"
+		//Setze nun das Enum, und damit auch die Default-StatusMessage
+		String sStatusMessageToSet = null;
+		if(StringZZZ.isEmpty(sStatusMessage)){
+			if(bStatusValue) {
+				sStatusMessageToSet = enumStatus.getStatusMessage();
+			}else {
+				sStatusMessageToSet = "NICHT " + enumStatus.getStatusMessage();
+			}			
+		}else {
+			sStatusMessageToSet = sStatusMessage;
 		}
 		
-		//+++ aus IStatusLocalUserMessageZZZ			
-		@Override 
-		public boolean setStatusLocal(Enum enumStatusIn, boolean bStatusValue, String sMessage) throws ExceptionZZZ {
-			boolean bFunction = false;
-			main:{
-				if(enumStatusIn==null) break main;
-				
-				IDummyTestObjectWithStatusLocalByInterfaceZZZ.STATUSLOCAL enumStatus = (IDummyTestObjectWithStatusLocalByInterfaceZZZ.STATUSLOCAL) enumStatusIn;
-				
-				bFunction = this.offerStatusLocal(enumStatus, bStatusValue, sMessage);
-			}//end main:
-			return bFunction;
-		}
-			
-		@Override 
-		public boolean setStatusLocalEnum(IEnumSetMappedStatusLocalZZZ enumStatusIn, boolean bStatusValue, String sMessage) throws ExceptionZZZ {
-			boolean bReturn = false;
-			main:{
-				if(enumStatusIn==null) break main;
-				
-				IDummyTestObjectWithStatusLocalByInterfaceZZZ.STATUSLOCAL enumStatus = (IDummyTestObjectWithStatusLocalByInterfaceZZZ.STATUSLOCAL) enumStatusIn;
-				
-				bReturn = this.offerStatusLocal(enumStatus, bStatusValue, sMessage);
-			}//end main:
-			return bReturn;
-		}
+		String sLog = ReflectCodeZZZ.getPositionCurrent() + "Verarbeite sStatusMessageToSet='" + sStatusMessageToSet + "'";
+		this.logProtocol(sLog);
 
-		@Override
-		public boolean queryOfferStatusLocalCustom() throws ExceptionZZZ {
-			return true;
-		}				
+		//Falls eine Message extra uebergeben worden ist, ueberschreibe...
+		if(sStatusMessageToSet!=null) {
+			sLog = ReflectCodeZZZ.getPositionCurrent() + "Setze sStatusMessageToSet='" + sStatusMessageToSet + "'";
+			this.logProtocol(sLog);
+		}
+		//Merke: Dabei wird die uebergebene Message in den speziellen "Ringspeicher" geschrieben, auch NULL Werte...
+		this.offerStatusLocalEnum(enumStatus, bStatusValue, sStatusMessageToSet);
+		
+		
+		
+		//Falls irgendwann ein Objekt sich fuer die Eventbenachrichtigung registriert hat, gibt es den EventBroker.
+		//Dann erzeuge den Event und feuer ihn ab.	
+		if(this.getSenderStatusLocalUsed()==null) {
+			sLog = ReflectCodeZZZ.getPositionCurrent() + "Would like to fire event '" + enumStatus.getAbbreviation() + "', but no objEventStatusLocalBroker available, any registered?";
+			this.logProtocol(sLog);		
+			break main;
+		}
+		
+		//Erzeuge fuer das Enum einen eigenen Event. Die daran registrierten Klassen koennen in einer HashMap definieren, ob der Event fuer sie interessant ist.		
+		sLog = ReflectCodeZZZ.getPositionCurrent() + "Erzeuge Event fuer '" + sStatusName + "'";		
+		this.logProtocol(sLog);
+		IEventObjectStatusLocalZZZ event = new EventObjectStatusLocalZZZ(this,enumStatus, bStatusValue);			
+		
+		//### GGFS. noch weitere benoetigte Objekte hinzufuegen............
+		//...
+		
 				
-		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+		//Feuere den Event ueber den Broker ab.
+		sLog = ReflectCodeZZZ.getPositionCurrent() + "Fires event '" + enumStatus.getAbbreviation() + "'";
+		this.logProtocol(sLog);
+		this.getSenderStatusLocalUsed().fireEvent(event);
+				
+		bFunction = true;				
+	}	// end main:
+	return bFunction;
+	}
+	
+	
+	
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	//+++ SET STATUS LOCAL, alle Varianten, gecasted auf dieses Objekt
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	@Override
+	public boolean setStatusLocal(Enum enumStatusIn, boolean bStatusValue) throws ExceptionZZZ {
+		boolean bFunction = false;
+		main:{
+			if(enumStatusIn==null) break main;
+			
+			IDummyTestObjectWithStatusLocalByInterfaceZZZ.STATUSLOCAL enumStatus = (IDummyTestObjectWithStatusLocalByInterfaceZZZ.STATUSLOCAL) enumStatusIn;
+							
+			bFunction = this.offerStatusLocal(enumStatus, bStatusValue, null);
+		}//end main:
+		return bFunction;
+	}
+		
+	/* (non-Javadoc)
+	 * @see basic.zBasic.AbstractObjectWithStatusZZZ#setStatusLocalEnum(basic.zBasic.util.abstractEnum.IEnumSetMappedStatusZZZ, boolean)
+	 */
+	@Override 
+	public boolean setStatusLocalEnum(IEnumSetMappedStatusLocalZZZ enumStatusIn, boolean bStatusValue) throws ExceptionZZZ {
+		boolean bReturn = false;
+		main:{
+			if(enumStatusIn==null) break main;
+
+			IDummyTestObjectWithStatusLocalByInterfaceZZZ.STATUSLOCAL enumStatus = (IDummyTestObjectWithStatusLocalByInterfaceZZZ.STATUSLOCAL) enumStatusIn;
+			
+			bReturn = this.offerStatusLocal(enumStatus, bStatusValue, null);
+		}//end main:
+		return bReturn;
+	}
+	
+	//+++ aus IStatusLocalUserMessageZZZ			
+	@Override 
+	public boolean setStatusLocal(Enum enumStatusIn, boolean bStatusValue, String sMessage) throws ExceptionZZZ {
+		boolean bFunction = false;
+		main:{
+			if(enumStatusIn==null) break main;
+			
+			IDummyTestObjectWithStatusLocalByInterfaceZZZ.STATUSLOCAL enumStatus = (IDummyTestObjectWithStatusLocalByInterfaceZZZ.STATUSLOCAL) enumStatusIn;
+			
+			bFunction = this.offerStatusLocal(enumStatus, bStatusValue, sMessage);
+		}//end main:
+		return bFunction;
+	}
+		
+	@Override 
+	public boolean setStatusLocalEnum(IEnumSetMappedStatusLocalZZZ enumStatusIn, boolean bStatusValue, String sMessage) throws ExceptionZZZ {
+		boolean bReturn = false;
+		main:{
+			if(enumStatusIn==null) break main;
+			
+			IDummyTestObjectWithStatusLocalByInterfaceZZZ.STATUSLOCAL enumStatus = (IDummyTestObjectWithStatusLocalByInterfaceZZZ.STATUSLOCAL) enumStatusIn;
+			
+			bReturn = this.offerStatusLocal(enumStatus, bStatusValue, sMessage);
+		}//end main:
+		return bReturn;
+	}
+
+	@Override
+	public boolean queryOfferStatusLocalCustom() throws ExceptionZZZ {
+		return true;
+	}				
+			
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 }
