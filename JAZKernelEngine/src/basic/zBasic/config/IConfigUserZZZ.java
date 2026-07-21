8b95b53c0c6abe27c3f8db111a23a8709ac0bbde
firak01 <firak01@t-online.de>
2026-07-21 20:40:00 +0200

    IConfigUserZZZ eingeführt

---
+++ JAZKernelEngine/src/basic/zBasic/config/IConfigUserZZZ.java
@@ -0,0 +1,8 @@
+package basic.zBasic.config;
+
+import basic.zBasic.ExceptionZZZ;
+
+public interface IConfigUserZZZ {
+	public IConfigZZZ getConfiguration() throws ExceptionZZZ;
+	public void setConfiguration(IConfigZZZ objConfig) throws ExceptionZZZ;
+}
