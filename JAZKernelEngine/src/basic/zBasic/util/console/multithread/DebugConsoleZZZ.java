f0da8ec645cbac465287067f0a5e4b7e925ff310
firak01 <firak01@t-online.de>
2026-08-17 23:09:52 +0200

    in Arbeit

--- JAZKernelEngine/src/basic/zBasic/util/console/multithread/DebugConsoleZZZ.java
+++ JAZKernelEngine/src/basic/zBasic/util/console/multithread/DebugConsoleZZZ.java
@@ -13,16 +13,19 @@ public class DebugConsoleZZZ {
 			//Merke: Hier ohne Menü fehlt so etwas wie...
 			//TODOGOON20260817;//Mache hier ein mini-Menü
 			//IKeyPressThreadZZZ objKeyPressThread = new ConsoleUserThreadZZZ(objConsole);
+			
+			TODOGOON20260817;//Der Zähler unten soll zur Demonstration herauskommen
+			                 //KeyPressThreadDefault ruft nicht objConsoleUserStartable.startit(...) auf.
 			IKeyPressThreadZZZ objKeyPressThread = new KeyPressThreadDefaultZZZ(objConsole, 100);
 			
 			objConsole.setKeyPressThread(objKeyPressThread);
 						
 			//IConsoleUserStartableZZZ objConsoleUser = new DummyConsoleUserStartableZZZ(objConsole);			
-			DummyConsoleUserStartableZZZ objConsoleUser = new DummyConsoleUserStartableZZZ(objConsole);			
-			objConsole.setConsoleUserStartableObject(objConsoleUser);
+			DummyConsoleUserStartableZZZ objConsoleUserStartable = new DummyConsoleUserStartableZZZ(objConsole);			
+			objConsole.setConsoleUserStartableObject(objConsoleUserStartable);
 			objConsole.start();
 			
-			Syso.println("Counter: " + objConsoleUser.getCounter());
+			Syso.println("Counter: " + objConsoleUserStartable.getCounter());
 		} catch (ExceptionZZZ e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
